851c5faf368942c2976470544bed79269cf308ad
Sams <nicholettesams@msn.com>
2018-12-26 14:52:44 -0500

    fix path to routes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./routes/apiRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
