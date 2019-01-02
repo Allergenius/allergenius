ae78cd37ca844eab193ef4f348a86d5e91a75b7d
NoPlanet_NoParty <eclancy11@gmail.com>
2019-01-01 19:33:46 -0500

    styling

--- client/src/pages/Forms/ReactionForm.js
+++ client/src/pages/Forms/ReactionForm.js
@@ -156,10 +156,9 @@ class ReactionForm extends Component {
 		
 		return (
 			<div className='container container-fluid'>
-				<div className='lead p-3 border bg-light text-center'>This app is not intended to replace medical care. If you are having an emergency, dial 911</div>
+				<div className='lead p-3 mb-2 border bg-light text-center'>This app is not intended to replace medical care. If you are having an emergency, dial 911</div>
 				<form className="container form-group m-4" onSubmit={this.handleFormSubmit}>
 					<h3 className="text-center p-4">Reaction Entry Form</h3>
-					
 					<h6>Type current date and time:</h6>
 					<Input
 						inputType={'text'}
