6699ec999aa264c691261626e3136ef399bcc3ed
NoPlanet_NoParty <eclancy11@gmail.com>
2019-01-17 17:09:51 -0500

    working

--- client/src/components/from-original/ListItem/ListItem.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListItem = props => (
-    <li id={props.id} key={props.id}>{props.date} {props.notes} {props.severity}</li>
-)
-
-export default ListItem;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express")
 var cors = require("cors")
 var bodyParser = require("body-parser")
+var Users = require('./routes/Users')
+const routes = require("./routes/apiRoutes")
 var app = express()
 var port = process.env.PORT || 5000
 
@@ -8,9 +10,10 @@ app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-var Users = require('./routes/Users')
-
 app.use('/users', Users)
+
+// allergenius
+app.use(routes)
 
 app.listen(port, () => {
     console.log("Server is running on port: " + port)
