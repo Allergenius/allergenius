8a64789023d16f6b35d709c49ae342020523de52
Sams <nicholettesams@msn.com>
2019-01-19 10:28:46 -0500

    add jaws connection for sequalize

--- config/db.js
+++ config/db.js
@@ -1,18 +1,15 @@
 const Sequelize = require("sequelize")
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + "/../config/config.json")[env];
 const db = {}
-const sequelize = new Sequelize("allergenius_db", "root", "root", {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: '3306',
-    operatorsAliases: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-})
+
+if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
+  
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
