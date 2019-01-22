92c35658050606310346bfffe9139ad5a40f3300
NoPlanet_NoParty <eclancy11@gmail.com>
2019-01-22 14:56:19 -0500

    remove unused code before dev merge

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,6 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
-// const localConfig = {
-//   host: process.env.JAWS_HOST,
-//   user: process.env.JAW_USER,
-//   password: process.env.JAWS_PW,
-//   database: process.env.JAWS_DATABASE,
-//   port: 3306
-// }
-
-// const connection = process.env.JAWSDB_URL ? mysql.createConnection(process.env.JAWSDB_URL) : mysql.createConnection(localConfig)
-
 // Jaws database
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
--- config/db.js
+++ config/db.js
@@ -5,38 +5,6 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
-// const localConfig = {
-//     host: process.env.JAWS_HOST,
-//     user: process.env.JAW_USER,
-//     password: process.env.JAWS_PW,
-//     database: process.env.JAWS_DATABASE,
-//     port: 3306,
-//     dialect: mysql,
-//     operatorsAliases: false,
-
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// }
-
-
-
-// var sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(process.env.JAWS_DATABASE, process.env.JAW_USER, process.env.JAWS_PW, {
-//     host: process.env.JAWS_HOST,
-//     dialect: mysql,
-//     port: 3306,
-//     operatorsAliases: false,
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// })
-console.log(process.env.JAWS_HOST)
 
 if (process.env.JAWSDB_URL){
     var sequelize = new Sequelize(process.env.JAWSDB_URL)
