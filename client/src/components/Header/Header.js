67f839e5dd1506c10c9816950aa0e96936eb5591
NoPlanet_NoParty <eclancy11@gmail.com>
2019-01-24 12:33:05 -0500

    styling changes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = props => (
-    <div className="header-styling mt-5 text-center">
+    <div className="header-styling mt-2 text-center">
         <p className="text-capitalize">Welcome {props.username}! </p>
     </div>
 )
--- client/src/components/Warning/Warning.js
+++ client/src/components/Warning/Warning.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Warning = () => (
-   <div className="warning-styling rounded text-center" role="alert">
+   <div className="warning-styling rounded text-center alert" role="alert">
       <p className="content-1"><strong>Always call 911 in the event of an emergency.</strong></p>
       <p className="warning-text">This app is not intended to replace medical care&mdash;instead, consult your physician.</p>
    </div>
