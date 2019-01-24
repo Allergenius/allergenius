b2c2876bba1806d1d8a1f129c5a21de6e3853ab7
NoPlanet_NoParty <eclancy11@gmail.com>
2019-01-24 12:23:11 -0500

    styling updates

--- client/src/components/Warning/Warning.js
+++ client/src/components/Warning/Warning.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Warning = () => (
    <div className="warning-styling rounded text-center" role="alert">
       <p className="content-1"><strong>Always call 911 in the event of an emergency.</strong></p>
-      <p className="lead">This app is not intended to replace medical care&mdash;instead, consult your physician.</p>
+      <p className="warning-text">This app is not intended to replace medical care&mdash;instead, consult your physician.</p>
    </div>
 );
 
